3ea87c4c2badfca1279f9b1694fab70f101217f9
Andrzej Borucki <borneq@example.com>
2014-01-31 22:21:47 +0100

    onClick button text changed

--- src/com/example/heregpslocation/MainActivity.java
+++ src/com/example/heregpslocation/MainActivity.java
@@ -3,13 +3,20 @@ package com.example.heregpslocation;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Button button;
+		button = (Button) findViewById(R.id.button1);
+		button.setOnClickListener(this);
 	}
 
 	@Override
@@ -17,6 +24,13 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		TextView textView;
+		textView = (TextView) findViewById(R.id.textView1);
+		textView.setText("12345");
 	}
 
 }
